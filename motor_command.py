08c850b2981ef7a313a93d38d3e68a39e570171c
lmtran <lmtran@bu.edu>
2025-04-13 23:06:38 -0400

    Create motor_command.py

---
+++ motor_command.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+'''Node specification for teleoperation of the robot'''
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+import robot_model as rm
+import me416_utilities as mu
+
+class MotorCommand(Node):
+    '''
+    A node for teleoperation of the robot
+    '''
+    def __init__(self):
+        super().__init__('motor_command')
+        self.motor_left = mu.MotorSpeedLeft()
+        self.motor_right = mu.MotorSpeedRight()
+        self.subscriber = self.create_subscription(Twist, 'cmd_vel', self.cmd_vel_callback, 10)
+        self.publisher = self.create_publisher(Twist, 'motor_speeds', 10)
+
+    def cmd_vel_callback(self, msg):
+        """ callback for cmd_vel """
+        # pull linear and angular speeds from Twist msg
+        speed_linear = msg.linear.x
+        speed_angular = msg.angular.z
+        # convert to left and right speeds
+        speed_left, speed_right = rm.twist_to_speeds(speed_linear, speed_angular)
+        # set acquired speeds to motor
+        self.motor_left.set_speed(speed_left)
+        self.motor_right.set_speed(speed_right)
+
+        self.get_logger().info(f'Left Motor Speed: {speed_left}, Right Motor Speed: {speed_right}')
